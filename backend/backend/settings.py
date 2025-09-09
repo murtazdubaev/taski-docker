55197aa7adbcbf5ea3c7358ce14bc4439a8f6b21
Murtaz Dubaev <git config --global user.email stas.basov@whatever.com>
2025-09-09 18:35:28 +0300

    add deploy2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '89.169.175.224,' 'https://todotaski.hopto.org']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '89.169.175.224', 'https://todotaski.hopto.org']
 
 
 # Application definition
